1cf6fb32289f0b109c4d52315caf6f74b6952a7d
김예은 <dfeb23rd@gmail.com>
2025-07-19 21:24:39 +0900

    feat: 18강 로그인 모달에서 발생하는 문제 해결하기 (router.replace)

--- my-app/src/app/(beforeLogin)/i/flow/login/page.tsx
+++ my-app/src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,8 +1,5 @@
-import LoginModal from "@/app/(beforeLogin)/_component/Login";
+import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
 
 export default function Page() {
-  
-  return (
-    <LoginModal />
-  );
+  return <LoginModal />;
 }
--- my-app/src/app/(beforeLogin)/login/page.tsx
+++ my-app/src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,20 @@
-import { redirect } from "next/navigation";
+/**
+ * 서버 클라이언트로 리다이렉트 할 시엔 인터셉팅 라우트가 제대로 안되는 이슈 발생
+ */
+// import { redirect } from "next/navigation";
+
+// export default function Login() {
+//   redirect("/i/flow/login");
+// }
+
+"use client";
+
+import { useRouter } from "next/navigation";
+import Main from "@/app/(beforeLogin)/_component/Main";
 
 export default function Login() {
-  redirect("/i/flow/login");
+  const router = useRouter();
+
+  router.replace("/i/flow/login");
+  return <Main />;
 }
