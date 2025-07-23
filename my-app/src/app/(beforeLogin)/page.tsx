b9d5ab4e5bfde4f2d9b0048ba57e280f96650fb3
김예은 <dfeb23rd@gmail.com>
2025-07-23 21:11:31 +0900

    feat: 코드 정리

--- my-app/src/app/(beforeLogin)/page.tsx
+++ my-app/src/app/(beforeLogin)/page.tsx
@@ -1,7 +1,3 @@
-import styles from "@/app/page.module.css";
-import Image from "next/image";
-import zLogo from "../../../public/zlogo.png";
-import Link from "next/link";
 import Main from "./_component/Main";
 
 export default function Page() {
