3e9362303172c3e5b7f6abe8809e52a39f58ff86
yeeun98 <dfeb23rd@gmail.com>
2025-08-05 17:09:04 +0900

    feat: photo modal / install faker | 강의 30강

--- my-app/src/app/(afterLogin)/_component/Post.tsx
+++ my-app/src/app/(afterLogin)/_component/Post.tsx
@@ -4,12 +4,17 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
-import PostArticle from './PostArticle';
+import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
+import {faker} from '@faker-js/faker';
+import PostImages from "@/app/(afterLogin)/_component/PostImages";
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
 
-export default function Post() {
+type Props = {
+  noImage?: boolean
+}
+export default function Post({ noImage }: Props) {
   const target = {
     postId: 1,
     User: {
@@ -19,15 +24,24 @@ export default function Post() {
     },
     content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any[],
   }
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      {imageId: 1, link: faker.image.urlLoremFlickr()},
+      {imageId: 2, link: faker.image.urlLoremFlickr()},
+      {imageId: 3, link: faker.image.urlLoremFlickr()},
+      {imageId: 4, link: faker.image.urlLoremFlickr()},
+    )
+  }
+
   return (
     <PostArticle post={target}>
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
           <Link href={`/${target.User.id}`} className={style.postUserImage}>
             <img src={target.User.image} alt={target.User.nickname}/>
-            <div className={style.postShade} />
+            <div className={style.postShade}/>
           </Link>
         </div>
         <div className={style.postBody}>
@@ -43,10 +57,10 @@ export default function Post() {
             <span className={style.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}>
-
+          <div>
+            <PostImages post={target} />
           </div>
-          <ActionButtons />
+          <ActionButtons/>
         </div>
       </div>
     </PostArticle>
